824d6fa85eb70d490022cf791ba5a636f31d5145
ansamlahham <>
2021-04-14 12:46:11 +0000

    lab04

---
+++ lab04.js
@@ -0,0 +1,18 @@
+
+var movienumber=6
+if(movienumber===6){
+  console.log('the movie is the greatest showman');
+}else{
+  console.log('the movie is not the greatest showman');
+}
+
+var usermovietype =prompt('please enter your movie type');
+if (usermovietype =='drama'){
+alert('you will love this movie');
+document.write('<img src="https://hbr.org/resources/images/article_assets/2020/09/A_Sep20_14_1189155141.jpg">');
+}else{
+  alert('this movie is not your type');
+document.write('<img src="https://alcoholismresearch.org/wp-content/uploads/2013/11/businessman-thumbs-down-90836525-777x437.jpg">');
+}
+var username=prompt('please enter your name')
+console.log(username);
