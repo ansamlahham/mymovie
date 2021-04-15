9d67995962be954df55f4ee0cd471d3da433b7b3
ansamlahham <>
2021-04-15 11:07:21 +0000

    lab05

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # welcome to our website 
+
+# My partner is aya and this is here work [link](https://ayaabunajm.github.io/mymovie/index)
---
+++ lab04.js
@@ -0,0 +1,23 @@
+
+var usermovietype =prompt('please enter your movie type');
+if (usermovietype =='drama'){
+alert('you will love this movie');
+}else if (usermovietype == 'action'){
+  alert('this movie is not your type');
+
+}
+while(usermovietype !== 'drama' && usermovietype !== 'action'){
+ usermovietype =prompt ('please enter the name drama or action');
+}
+var userchoose;
+
+if (usermovietype === 'drama'){
+  userchoose ='<img src="https://hbr.org/resources/images/article_assets/2020/09/A_Sep20_14_1189155141.jpg">'
+} else if (usermovietype === 'action') {
+userchoose = '<img src="https://alcoholismresearch.org/wp-content/uploads/2013/11/businessman-thumbs-down-90836525-777x437.jpg">'
+}
+var imagesNumber= prompt('How many image do you want???');
+
+for(var i=0 ; i < imagesNumber; i++){
+  document.write(userchoose);
+}
